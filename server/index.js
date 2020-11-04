9a545ddaddc5c08f18ffe4f5e7877b8e8da4e63d
groana <groana7@gmail.com>
2020-11-04 17:03:21 -0500

    error handling in express

--- server/index.js
+++ server/index.js
@@ -2,14 +2,26 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const path = require('path');
+const port = process.env.PORT || 1337;
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api', require('./api'));
+
 app.use(express.static(path.join(__dirname, '../public')));
 
-const PORT = 1337
-app.listen(PORT, () => {
-  console.log(`Serving silly sounds in PORT ${PORT}`)
-})
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+app.listen(port, () => {
+  console.log(`Serving silly sounds in PORT ${port}`);
+});
