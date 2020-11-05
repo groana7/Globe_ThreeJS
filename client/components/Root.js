a968b8acf221484282ad1d66828a9947630565c7
groana <groana7@gmail.com>
2020-11-04 21:57:05 -0500

    globe animates with arrow buttons

--- client/components/Root.js
+++ client/components/Root.js
@@ -1,18 +1,10 @@
 //import three.js
-import React from 'react';
 const THREE = require('three');
 
 //export stateless React component
 export default function Root() {
   return null;
 }
-
-// export default class Root extends React.Component {
-//   render() {
-//     // return <div>HELLO</div>
-//     return null
-//   }
-// }
 
 const container = document.querySelector('#container');
 
@@ -38,17 +30,23 @@ const NEAR = 0.1;
 const FAR = 10000;
 
 // then instantiate the camera
-const camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+const camera =
+new THREE.PerspectiveCamera(
+    VIEW_ANGLE,
+    ASPECT,
+    NEAR,
+    FAR
+);
 
 // set its position
-camera.position.set(0, 0, 500);
+camera.position.set( 0, 0, 500 );
 
 // create the scene
 const scene = new THREE.Scene();
 
 // set the background color - black
 // how to change the background color?
-scene.background = new THREE.Color(0x000);
+scene.background = new THREE.Color( 0x000 );
 
 // add the camera to the scene
 scene.add(camera);
@@ -72,22 +70,26 @@ scene.add(globe);
 // create our sphere and its texture, and mesh them together using three.js’s TextureLoader
 var loader = new THREE.TextureLoader();
 
-loader.load('https://eoimages.gsfc.nasa.gov/images/imagerecords/57000/57735/land_ocean_ice_cloud_2048.jpg', function (texture) {
-  // Create the sphere
-  var sphere = new THREE.SphereGeometry(RADIUS, SEGMENTS, RINGS);
-  // Map the texture to the material.
-  var material = new THREE.MeshBasicMaterial({ map: texture, overdraw: 0.5 });
-  // Create a new mesh with sphere geometry.
-  var mesh = new THREE.Mesh(sphere, material);
+// loader.load( 'land_ocean_ice_cloud_2048.jpg', function ( texture ) {
+loader.load( 'slavetrademaplarge.jpg', function ( texture ) {
+  //create the sphere
+  var sphere = new THREE.SphereGeometry( RADIUS, SEGMENTS, RINGS );
 
-  // Add mesh to globe
+  //map the texture to the material. Read more about materials in three.js docs
+  var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
+
+  //create a new mesh with sphere geometry. 
+  var mesh = new THREE.Mesh( sphere, material );
+
+  //add mesh to globe group
   globe.add(mesh);
-});
+} );
 
 globe.position.z = -300;
 
 // Create light
-const pointLight = new THREE.PointLight(0xFFFFFF);
+const pointLight =
+new THREE.PointLight(0xFFFFFF);
 
 // position the light
 pointLight.position.x = 10;
@@ -96,3 +98,66 @@ pointLight.position.z = 400;
 
 // add the light to the scene
 scene.add(pointLight);
+
+function update () {
+
+  //Render:
+  renderer.render(scene, camera);
+  // Schedule the next frame:
+  requestAnimationFrame(update);  
+}
+// Schedule the first frame:
+requestAnimationFrame(update);
+
+//Rotate on Arrow Key Press
+
+//setting up our rotation based on arrow key
+function animationBuilder(direction) {
+  return function animateRotate() {
+    switch (direction) {
+      case 'up':
+        globe.rotation.x -= 0.2;
+        break;
+      case 'down':
+        globe.rotation.x += 0.2;
+        break;
+      case 'left':
+        globe.rotation.y -= 0.2;
+        break;
+      case 'right':
+        globe.rotation.y += 0.2;
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+// store animation call in directions object
+var animateDirection = {
+  up: animationBuilder('up'),
+  down: animationBuilder('down'),
+  left: animationBuilder('left'),
+  right: animationBuilder('right')
+}
+
+// define the listener function we want to occur after a key has been pressed
+function checkKey(e) {
+  e = e || window.event;
+  e.preventDefault();
+ 
+  //based on keycode, trigger appropriate animation:
+  if (e.keyCode == '38') {
+    animateDirection.up();
+  } else if (e.keyCode == '40') {
+    animateDirection.down();
+  } else if (e.keyCode == '37') {
+    animateDirection.left();
+  } else if (e.keyCode == '39') {
+    animateDirection.right();
+  }
+}
+
+// on key press invoke the above listener
+document.onkeydown = checkKey;
+
